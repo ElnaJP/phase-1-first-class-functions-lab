06d7ba00873bc3005a02fadac14af53dfaa87f27
Elna JP <eriosdd@gmail.com>
2022-07-27 21:23:38 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const  scubaDrivers = (['Antonia', 'Nuru', 'Amari', 'Mo']);
+const returnFirstTwoDrivers = function scubaDrivers () {
+return (['Antonia', 'Nuru']);
+};
+
+const returnLastTwoDrivers = function scubaDrivers () {
+    return (['Amari', 'Mo']);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers,];
+ 
+function createFareMultiplier(number)  {
+ return function (fare) {
+return fare * number
+}
+ }
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(scubaDrivers, fn) {
+    return fn(scubaDrivers);
+}
+
